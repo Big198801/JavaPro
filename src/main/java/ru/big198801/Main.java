5f3e04eb68960a3a462be58e5be80a5b87e49b5c
Nikolay Afanasev <bigniko@mail.ru>
2025-09-25 18:11:19 +0300

    HW3 branch created

--- src/main/java/ru/big198801/Main.java
+++ src/main/java/ru/big198801/Main.java
@@ -3,5 +3,6 @@ package ru.big198801;
 public class Main {
     public static void main(String[] args) {
         System.out.println("JavaVTB");
+        System.out.println("HW3_CustomThreadPool");
     }
 }
